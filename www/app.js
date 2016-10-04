ea531c28489a2f988ec6b7b30eb7a65fbf8f52d6
John Tsinonis <jtsinoni@comcast.net>
2016-10-04 08:15:04 -0400

    added empty store and forward views

--- www/app.js
+++ www/app.js
@@ -6,8 +6,14 @@ dmlesMobileApp.config(function ($routeProvider) {
          .when('/', {
              templateUrl: 'app/mqtt/views/mqtt.html',
              controller: 'mqttController'
+         }).
+         when('/store', {
+             templateUrl: 'app/store-forward/views/store.html'
+         }).
+         when('/forward', {
+             templateUrl: 'app/store-forward/views/forward.html'
          })
          .otherwise({
              redirectTo: '/'
-         });
+        });
  });
